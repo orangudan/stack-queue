f0ed6161a846c049d453aa7522d6f84802eac17a
Daniel Sadeghi <dsadeghi@Daniels-Air-2.attlocal.net>
2022-03-26 03:43:45 -0700

    init commit

---
+++ src/main/com/dsadeghi/stackqueue/ArrayListStack.java
@@ -0,0 +1,37 @@
+package com.dsadeghi.stackqueue;
+
+import my.library.StackInterface;
+
+public class ArrayListStack <E> implements StackInterface<E> {
+
+    @Override
+    public boolean empty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Object peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object push(Object obj) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean isPalindrome() {
+        // TODO Implement
+        return false;
+    }
+    
+
+}
